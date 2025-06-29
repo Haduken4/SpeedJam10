33113f4ce153ceab213792141d32f3266f74aa3f
BarBenzvi <bar_bar@live.com>
2025-06-29 00:01:16 -0700

    Import assets and started obstacles

--- Unity Project/SpeedJam10/Assets/Scripts/Environment/Obstacle.cs
+++ Unity Project/SpeedJam10/Assets/Scripts/Environment/Obstacle.cs
@@ -1,7 +1,13 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class Obstacle : MonoBehaviour
 {
+    public SplineContainer MyPath = null;
+    public bool StartFromWorldPos = true;
+    public float MoveSpeed = 0.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
